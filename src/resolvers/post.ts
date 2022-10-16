84f046f15efbc9a8df0f315dab84006f2ee65372
edge2992 <torune.summer@gmail.com>
2022-10-16 22:35:43 +0900

    fix updoot to call mutiple times

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -42,21 +42,39 @@ export class PostResolver {
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
-    // Updoot.insert({ userId, postId, value: realValue });
-    await AppDataSource.query(
-      `
-      START TRANSACTION;
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-      insert into updoot ("userId", "postId", value)
-      values (${userId}, ${postId}, ${realValue});
+    // the user has voted on the post before
+    // and they are changing their vote
+    if (updoot && updoot.value !== realValue) {
+      await AppDataSource.transaction(async (em) => {
+        await em.query(`
+      update updoot
+      set value = $1
+      where "postId" = $2 and "userId" = $3
+        `, [realValue, postId, userId]);
 
+        await em.query(`
       update post 
-      set points = points + ${realValue}
-      where id = ${postId};
+      set points = points + $1 
+      where id = $2;
+        `, [realValue * 2, postId]);
+      });
+    } else if (!updoot) {
+      // has never voted before
+      await AppDataSource.transaction(async (em) => {
+        em.query(`
+      insert into updoot ("userId", "postId", value)
+      values ($1, $2, $3);
+        `, [userId, postId, realValue]);
 
-      COMMIT;
-      `
-    )
+        await em.query(`
+      update post 
+      set points = points + $1 
+      where id = $2;
+        `, [realValue, postId]);
+      });
+    }
     return true;
   }
 
